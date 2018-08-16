71c3a0bae54b7ea300d983877c1727b2a2c2191e
mradi941 <lilly_aleffio@yahoo.com>
2018-08-16 03:02:04 +0300

    Cod refactorizat beta

--- Bowling.js
+++ Bowling.js
@@ -1,41 +1,35 @@
-var Bowling = function(){
+var Bowling = function() {
   this.rollsArray = [];
   this.scoresArray = [];
   this.rollCounter = 0;
-  this.frameCounter = 1;
+  this.cellCounter = 1;
   this.last_resolved_frame = 0;
 };
 
-Bowling.prototype.roll = function(rollScore){
+Bowling.prototype.roll = function( rollScore ){
   var self = this;
 
   function throwAlert(){
     alert("Illegal score");
   };
-// de tratat al 10-lea frame
-  // function isIllegalScore(){
-  //   return self.frameCounter % 2 === 0 && (rollScore + self.scoresArray[self.scoresArray.length - 1] > 10);
-  // };
-  
+
   function isIllegalScore() {
     var breaksLimit = rollScore + self.rollsArray[self.rollCounter - 1] > 10;
-    if(! (self.frameCounter > 18)) {
-
-      if(self.frameCounter % 2 === 1) {
-      // orice aruncare in prima celula a unui frame este valida
+    if (! ( self.cellCounter > 18 ) ) {
+      if ( self.cellCounter % 2 === 1) {
+// orice aruncare in prima celula a unui frame este valida
         return false;
       };
-    // o aruncare este valida in al 2-lea frame daca suma nu depaseste 10 popice
+// o aruncare este valida in al 2-lea frame daca suma nu depaseste 10 popice
     return rollScore + self.rollsArray[self.rollCounter - 1] > 10;
     } else {
-
-      if(self.frameCounter === 19) {
+      if ( self.cellCounter === 19 ) {
 
         return false;
       };
-      if(self.frameCounter === 20) {
+      if ( self.cellCounter === 20 ) {
         var wasStrike = self.rollsArray[self.rollCounter - 1] === 10;
-        return !wasStrike && breaksLimit;
+        return ! wasStrike && breaksLimit;
       };
       var wasStrike = self.rollsArray[self.rollCounter - 1] === 10;
       var wasSpare = self.rollsArray[self.rollCounter - 2] + self.rollsArray[self.rollCounter - 1] === 10;
@@ -43,83 +37,89 @@ Bowling.prototype.roll = function(rollScore){
     };
   };
   
-  if (isIllegalScore()){
+  if ( isIllegalScore() ) {
     throwAlert();
   } else {
-    self.rollsArray.push(rollScore);
-    console.log(rollScore);
-    self.registerRoll(rollScore);
+    self.rollsArray.push( rollScore );
+    console.log( rollScore );
+    self.registerRoll( rollScore );
   };
 };
 
-Bowling.prototype.registerRoll = function(rollScore){
+Bowling.prototype.registerRoll = function( rollScore ) {
   var self = this;
 
-  function tenthFrame (){
-    return self.frameCounter > 18;
+  function finalFrame () {
+    return self.cellCounter > 18;
   };
 
-  function isStrike(){
-    return rollScore === 10 && self.frameCounter % 2 === 1;
+  function isStrike() {
+    return rollScore === 10 && self.cellCounter % 2 === 1;
   };
 
-  function isFrameEnd(){
-    return self.frameCounter % 2 === 0;
+  function isFrameEnd() {
+    return self.cellCounter % 2 === 0;
   };
 
-  function isSpare(){
+  function isSpare() {
     return rollScore + self.rollsArray[self.rollCounter - 1] === 10;
   };
 
-  function decideSpare(){
-    if (isSpare()){
+  function decideSpare() {
+    if ( isSpare() ) {
       self.scoresArray.push('/');
     } else {
-      self.scoresArray.push(rollScore);
+      self.scoresArray.push( rollScore );
     };
   };
 
-  function get_current_frame(){
-  	return Math.floor(self.frameCounter / 2 ) + self.frameCounter % 2;
+  function get_current_frame() {
+  	return Math.floor( self.cellCounter / 2 ) + self.cellCounter % 2;
   }
 
-  function was_strike(){
-  	return self.frameCounter >= 3 && self.rollsArray[self.rollCounter - 1] === 10; // - 2?
+  function was_strike() {
+  	return self.cellCounter >= 3 && self.rollsArray[self.rollCounter - 1] === 10; // - 2?
   };
   
-  function was_spare(){
-  	return self.frameCounter >= 3 && !was_strike() && self.rollsArray[self.rollCounter - 1] + self.rollsArray[self.rollCounter - 2] === 10;
+  function was_spare() {
+  	return self.cellCounter >= 3 && ! was_strike() && self.rollsArray[self.rollCounter - 1] + self.rollsArray[self.rollCounter - 2] === 10;
   };
   
-  function was_double_strike(){
+  function was_double_strike() {
 		return self.last_resolved_frame < get_current_frame() - 2;
   };
   
-  function calculate_last_resolved_frame(){
+  function calculate_last_resolved_frame() {
+
   	
-    if ( self.frameCounter === 20) {
-  		
-      if (self.rollsArray[self.rollCounter - 1] === 10){
+    if ( self.cellCounter === 20 ) {
+      if ( self.rollsArray[self.rollCounter - 1] === 10 ){
   			self.last_resolved_frame = get_current_frame() - 1;
   			return;
   		}
-    
+
+      if (self.rollsArray[self.rollCounter - 1] !== 10){
+        self.last_resolved_frame = get_current_frame();
+      }
   	}
-  	if ( self.frameCounter == 21) {
+  	if ( self.cellCounter == 21 ) {
   		self.last_resolved_frame = 10;
   		return; 
   	}
-	if(self.frameCounter % 2 === 1){
-		if (was_spare()){
-			self.last_resolved_frame = get_current_frame() - 1;
-			return; 
-		};
-		if (was_double_strike()){
-			self.last_resolved_frame = get_current_frame() - 2;
-			return;
-		};
-	}  	else { // sunt pe celula para in frame
-			if(!isSpare() ){ // caz particular double strike
+
+	  if( self.cellCounter % 2 === 1 ){
+
+
+  		if ( was_spare() ) {
+  			self.last_resolved_frame = get_current_frame() - 1;
+  			return; 
+  		};
+  		if ( was_double_strike() ) {
+  			self.last_resolved_frame = get_current_frame() - 2;
+  			return;
+  		};
+	  } else { // sunt pe celula para in frame
+			if( !isSpare() ){ // caz particular double strike
 				console.log("Aici am prezis");
 				self.last_resolved_frame = get_current_frame();
 				return; 
@@ -130,85 +130,81 @@ Bowling.prototype.registerRoll = function(rollScore){
 			}
 		}
   };
-  function pushScore(){
-    self.scoresArray.push(rollScore);
+  function pushScore() {
+    self.scoresArray.push( rollScore );
   };
-// Ordine gresita if-uri, nu afiseaza X in al 10-lea frame
 
   var strike_counter = 0 ;
-  if (tenthFrame()){
-  	console.log("Am ajuns aici");
-    if (rollScore === 10) {
-         if(self.frameCounter === 19 || self.rollsArray[self.rollCounter - 1] !== 0){
+  if ( finalFrame() ){
+    if ( rollScore === 10 ) {
+         if( self.cellCounter === 19 || self.rollsArray[self.rollCounter - 1] !== 0 ) {
          	 self.scoresArray.push('X');
          }
-         else{
+         else {
          	self.scoresArray.push("/");
          }
          // inca o conditie 
-    } else {console.log("Am ajuns acolo");
-      if(self.frameCounter !== 19) {
-      if(isSpare() && rollScore !== 0) {
-        if( self.rollsArray[self.rollCounter - 1] + self.rollsArray[self.rollCounter - 2] === 10){
+    } else {
+      if( self.cellCounter !== 19 ) {
+        if(isSpare() && rollScore !== 0) {
+          if( self.rollsArray[self.rollCounter - 1] + self.rollsArray[self.rollCounter - 2] === 10 ) {
+            pushScore();
+          }
+          self.scoresArray.push('/');  
+        } else {
           pushScore();
-        }
-        self.scoresArray.push('/');  
-      } else {
-
-        pushScore();
-      };
+        };
     }	else {
     		pushScore();
     } ;
   };
-} else if (isStrike()){
+} else if ( isStrike() ) {
+
 // De bagat X si null in loc de 0
-    self.scoresArray.push('X');
-    self.scoresArray.push('-');
-    strike_counter ++;
-  } else if (isFrameEnd()){
+  self.scoresArray.push('X');
+  self.scoresArray.push('-');
+  strike_counter ++;
+  } else if ( isFrameEnd() ){
     decideSpare();
   } else {
     pushScore();
   };
 
   calculate_last_resolved_frame();
-  console.log("CF " + get_current_frame());
-  console.log("LRF "+self.last_resolved_frame);
-  self.frameCounter += 1 + strike_counter;
+  self.cellCounter += 1 + strike_counter;
   self.rollCounter ++;
 };
 
-Bowling.prototype.cumulativeScore = function(frameNumber){
+Bowling.prototype.cumulativeScore = function( frameNumber ){
   var self = this;
   var score = 0;
   var frameStart = 0;
 
-  function isStrike (){
+  function isStrike () {
     return self.rollsArray[frameStart] === 10;
   };
 
-  function isSpare (){
+  function isSpare () {
     return self.rollsArray[frameStart] + self.rollsArray[frameStart + 1] === 10;
   };
 
-  function frameTotal (){
+  function frameTotal () {
     return self.rollsArray[frameStart] + self.rollsArray[frameStart + 1] || 0;
   };
 
-  function strikeBonus (){
+  function strikeBonus () {
     return self.rollsArray[frameStart + 1] + self.rollsArray[frameStart + 2] || 0;
   };
 
-  function spareBonus (){
+  function spareBonus () {
     return self.rollsArray[frameStart + 2] || 0;
   };
 
-  for (var i = 0; i < frameNumber; i ++){
-    if(isStrike()){
+  for ( var i = 0; i < frameNumber; i ++ ) {
+    if( isStrike() ) {
       score += 10 + strikeBonus();
       frameStart ++;
-    } else if (isSpare()){
+    } else if ( isSpare() ){
       score += 10 + spareBonus();
       frameStart += 2;
     } else {
